f02b78cab3d2a347948871340f717588b4dab199
Nelyelim Valeria Morán <nelyelimmoran7@gmail.com>
2024-06-05 23:24:32 -0500

    Deployment

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+// Conectar base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada'))
+    .catch( error => console.log(error));
+
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+
+// Obtener el Año Actual
+app.use( (req, res, next) => {
+    const year = new Date();
+
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio= 'Agencia de Viajes';
+    next();
+});
+
+// Agregar Body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
+
+app.listen(port,() => {
+    console.log(`El Servidor esta funcionando en el puerto ${port}`)
+})
